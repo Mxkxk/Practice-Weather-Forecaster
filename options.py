a90026d7b06ee6c7abbe66a79865ddfd6dcae3a3
Fedoriak Maksym <74319751+Mxkxk@users.noreply.github.com>
2023-04-11 21:07:42 +0300

    Added new options for index input and resources

--- options.py
+++ options.py
@@ -7,3 +7,14 @@ WEATHER_NAME = "Прогноз погоди"
 MENU = ["Погода", "Історія", "Про програму", "Вийти"]
 WEATHER_API_KEY = '5efcc8b5b685d99dbcf01585a90702ef'
 GEOCODING = ''
+ICON = "resources/icon.png"
+HOUSES = "resources/houses.csv"
+#Input index form
+INDEX_LABEL = "Введіть індекс" 
+INDEX_PUSHBUTTON = "Підтвердити" 
+INDEX_RADIO_TITLE = "Виберіть час прогнозу"
+INDEX_RADIO_DAY = "На день"
+INDEX_RADIO_WEEK = "На тиждень"
+INDEX_MAX_VALUE = 99999
+#Message codes
+MESSAGE_ERROR = "Виникла помилка"
