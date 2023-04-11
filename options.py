3dbb46ddfaf26d01fd060af81385dc7f1bd08772
Fedoriak Maksym <74319751+Mxkxk@users.noreply.github.com>
2023-04-11 15:11:57 +0300

    Added names and keys

--- options.py
+++ options.py
@@ -1,4 +1,9 @@
 #File with custom values
 MAIN_NAME = "Weather Forecaster"
 MAIN_THEME = "theme.css"
+ABOUT_NAME = "Про програму"
+HISTORY_NAME = "Історія"
+WEATHER_NAME = "Прогноз погоди"
 MENU = ["Погода", "Історія", "Про програму", "Вийти"]
+WEATHER_API_KEY = '5efcc8b5b685d99dbcf01585a90702ef'
+GEOCODING = ''
