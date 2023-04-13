b8f7aa51d1d65d237f0e8b4c417e92b1dda3a1f4
Fedoriak Maksym <74319751+Mxkxk@users.noreply.github.com>
2023-04-13 12:57:32 +0300

    Update widgets.py
    Added saving and loading of history

--- widgets.py
+++ widgets.py
@@ -2,7 +2,7 @@ import sys
 import PySide6.QtWidgets as QW
 import PySide6.QtGui as QG
 import PySide6.QtCore as QC
-
+import json
 import options as CustomOptions
 
 
@@ -18,7 +18,9 @@ class About(QW.QWidget):
         self.text_about.setEnabled(False)
         self.text_about.setAlignment(QC.Qt.AlignmentFlag.AlignRight)
         about_add_text = lambda s: self.text_about.setText(self.text_about.toPlainText()+s)
-        about_add_text("\n\tQT:"+str(QC.qVersion()))
+        about_add_text("\n\t" + CustomOptions.ABOUT_TEXT)
+        about_add_text("\n\tВерсія QT:" + str(QC.qVersion()))
+        
         
 
         self.layout.addWidget(self.text_about)
@@ -45,9 +47,48 @@ class History(QW.QWidget):
         
         self.list = QW.QListView()
         self.layout.addWidget(self.list)
-
+        self.load_data = []
         self.setLayout(self.layout)
 
+        self.readHistory()
+
+    def read_file(self, name):
+            try:
+                with open(name, 'r') as f:
+                    return f.read()
+            except:
+                return None
+            
+    def errase_file(self, name):
+        try:
+            with open(name, 'w') as f:
+                f.write('')
+        except: pass
+    
+    def rewrite_file(self, name, text):
+        try:
+            with open(name, 'w') as f:
+                f.write(text)
+        except: pass
+
+    def write_file(self, name, text):
+        with open(name, 'a+') as f:
+            f.write(text)
+
+    def saveHistory(self, weather_data):        
+        if self.read_file(CustomOptions.HISTORY) is None:
+            self.write_file(CustomOptions.HISTORY, '[]')
+        data = json.loads(self.read_file(CustomOptions.HISTORY))
+        data.append(weather_data)
+        self.rewrite_file(CustomOptions.HISTORY, json.dumps(data))
+            
+    def readHistory(self):  
+        if self.read_file(CustomOptions.HISTORY) is None:
+            self.write_file(CustomOptions.HISTORY, '[]')
+        self.load_data = json.loads(self.read_file(CustomOptions.HISTORY))
+        print(self.load_data)
+
+        
 
     def setStyle(self):
         theme_file = None
@@ -62,32 +103,41 @@ class History(QW.QWidget):
 class Weather(QW.QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle(CustomOptions.MAIN_NAME + ": " + CustomOptions.ABOUT_NAME)
-        self.setStyle()       
-        
+        self.setWindowTitle(CustomOptions.MAIN_NAME + ": " + CustomOptions.WEATHER_NAME)
+        self.setStyle()
+        self.radio_type = ""
+        self.weather_widgets = []
+        self.layout = QW.QStackedLayout()
+        self.setLayout(self.layout)
+
     def inputForm(self):
 
         self.form = QW.QWidget()
         self.form.setWindowTitle(CustomOptions.INDEX_LABEL)
-        self.form.setMinimumHeight(200)
+        self.form.setMinimumHeight(250)
         self.form.setWindowIcon(QG.QPixmap(CustomOptions.ICON))
 
         self.form_layout = QW.QVBoxLayout()
 
-        self.edit_index = QW.QLineEdit()
-        self.edit_index.setValidator(QG.QIntValidator(0, CustomOptions.INDEX_MAX_VALUE))
+        self.edit_index = QW.QLineEdit()    
         self.edit_index.setPlaceholderText(CustomOptions.INDEX_LABEL)
         self.edit_index.setAlignment(QC.Qt.AlignmentFlag.AlignCenter)
 
         self.box_index = QW.QGroupBox(title=CustomOptions.INDEX_RADIO_TITLE)        
-        self.radio_day = QW.QRadioButton(text=CustomOptions.INDEX_RADIO_DAY)
-        self.radio_day.setChecked(True)
-        self.radio_week = QW.QRadioButton(text=CustomOptions.INDEX_RADIO_WEEK)
+        self.radio = {}
+        self.radio["index"] = QW.QRadioButton(text=CustomOptions.INDEX_RADIO_INDEX)
+        self.radio["index"].setChecked(True)
+        self.radio["city"] = QW.QRadioButton(text=CustomOptions.INDEX_RADIO_CITY)
+        #self.radio["location"] = QW.QRadioButton(text=CustomOptions.INDEX_RADIO_LOC)
 
         box_index_layout = QW.QVBoxLayout(self.box_index)
-        box_index_layout.addStretch(0)
-        box_index_layout.addWidget(self.radio_day)
-        box_index_layout.addWidget(self.radio_week)
+        box_index_layout.setAlignment(QC.Qt.AlignmentFlag.AlignBottom)
+        box_index_layout.addSpacerItem(QW.QSpacerItem(1, 50))
+        for k in self.radio.keys():
+            self.radio[k].clicked.connect(self.check_radios)
+            box_index_layout.addWidget(self.radio[k])
+        
+        self.check_radios()
 
         self.button_index = QW.QPushButton(text=CustomOptions.INDEX_PUSHBUTTON)
         
@@ -100,8 +150,49 @@ class Weather(QW.QWidget):
         self.form.setVisible(True)
         self.form.setStyleSheet(self.styleSheet())
 
-    
+    def generateLayout(self, data, city = ''):
+        self.weather_widgets_layout = QW.QHBoxLayout()
 
+        if data[0] == "empty":
+            for i in range(7):
+                self.weather_widgets_layout.addWidget(self.getWeatherWidget(data[1]))
+        else:
+            for d in data[1::]:
+                self.weather_widgets_layout.addWidget(self.getWeatherWidget(d))
+            w = QW.QWidget()
+            w.setLayout(self.weather_widgets_layout)
+            self.layout.addWidget(w)
+            self.layout.setCurrentIndex(self.layout.count()-1)
+           
+            
+
+    def getWeatherWidget(self, data):        
+        widget = QW.QWidget()
+        layout = QW.QVBoxLayout()
+
+        labels = []
+        labels.append(QW.QLabel(text='.'.join(data["Date"][::-1])))
+        labels.append(QW.QLabel(text=data["Conditions"]))
+        labels.append(QW.QLabel(text=data["Max"]))
+        labels.append(QW.QLabel(text=data["Min"]))
+        
+        QW.QLabel().setObjectName('forecast')
+
+        for l in labels:
+            l.setAlignment(QC.Qt.AlignmentFlag.AlignCenter)
+            l.setWordWrap(True)
+            l.setObjectName('forecast')
+            layout.addWidget(l)
+
+        widget.setLayout(layout)
+        return widget
+
+    @QC.Slot()
+    def check_radios(self):
+        for k in self.radio.keys():
+            if self.radio[k].isChecked():
+                self.radio_type = k
+                break
 
     def setStyle(self):
         theme_file = None
