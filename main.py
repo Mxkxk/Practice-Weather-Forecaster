362a35dc9fe42b018746443eae3d663b1685e7f8
Fedoriak Maksym <74319751+Mxkxk@users.noreply.github.com>
2023-04-16 02:28:15 +0300

    Update main.py
    Rewrited getGeocode method to locate city by its lat and lon. Rewrited getWeather method to iterate data from geocoding list. Added history deleting and live list update. Optimised check_index method

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ class MainWindow(QW.QWidget):
         self.setWindowIcon(QG.QPixmap(CustomOptions.ICON))
         #set main layout for app
 
-        self.generateLayout()      
+        self.generateLayout()
         self.setLayout(self.layout)
         
         self.setMenu()
@@ -55,6 +55,7 @@ class MainWindow(QW.QWidget):
 
         self.history = History()
         self.history.choose_button.clicked.connect(self.history_load)
+        self.history.delete_button.clicked.connect(self.history_delete)
 
         self.about = About()
         
@@ -75,29 +76,16 @@ class MainWindow(QW.QWidget):
         self.menus[CustomOptions.MENU[2]].clicked.connect(self.about_menu)
         self.menus[CustomOptions.MENU[3]].clicked.connect(self.close_menu)
         
-        self.layout.addLayout(self.menuBar)        
-
-    def getGeocode(self):
-        #Computing lat and lon of city
-        osm_df = pd.read_csv(CustomOptions.LOCATION, sep='\t')
-        osm_df = osm_df[(osm_df["state"] == self.data["Oblast"]) | osm_df["state"].isnull()]
-        try:
-            self.data["Location"] = osm_df[osm_df["name"].str.contains(self.data["City"], na=False)]
-            if len(self.data["Location"]["lon"]) < 1:
-                raise                       
-        except:
-            self.data["Location"] = osm_df[osm_df["alternative_names"].str.contains(self.data["City"], na=False)] 
-        if len(self.data["Location"]["lon"]) > 0:
-            lon = sum(self.data["Location"]["lon"])/len(self.data["Location"]["lon"])
-            lat = sum(self.data["Location"]["lat"])/len(self.data["Location"]["lat"] )
-            self.data["Location"] = {"Lat": lat, "Lon": lon}            
-        else:
-            Message(CustomOptions.MESSAGE_ERROR, "Місто не знайдено за індексом")
+        self.layout.addLayout(self.menuBar)
 
     def getWeather(self, type):
-        if type == "index" or type == "city":                       
-            url = "https:" + pr.quote(CustomOptions.SINOPTIK + self.data["City"].lower())
-            answer = re.findall(r'id="bd[1-9]">.*?&nbsp;</div>', ''.join(requests.get(url).text.split("\r\n")))            
+        if type == "location":
+            self.getGeocode()
+        else:
+            self.data["City"] = ['', self.data["City"]]
+        for i, city in enumerate(self.data["City"]):
+            url = "https:" + pr.quote(CustomOptions.SINOPTIK + city.lower())
+            answer = re.findall(r'id="bd[1-9]">.*?&nbsp;</div>', ''.join(requests.get(url).text.split("\r\n")))
             if len(answer) > 1:
                 self.data["Weather"] = []
                 for a in answer:
@@ -109,18 +97,36 @@ class MainWindow(QW.QWidget):
                     self.data["Weather"].append(data)
 
                 self.data["Weather"].insert(0, str(date.today()))
+                self.data["City"] = city
+                return True
+                    
+        Message(CustomOptions.MESSAGE_ERROR, "Місто не знайдено", self.styleSheet())
+        return False
 
-            else:
-                Message(CustomOptions.MESSAGE_ERROR, "Місто не знайдено", self.styleSheet())
-            
-
-        if type == "location":
-            self.getGeocode()   
+    def getGeocode(self):
+        #Computing lat and lon of city
+        osm_df = pd.read_csv(CustomOptions.LOCATION, sep='\t')
+        #osm_df = osm_df[(osm_df["state"] == self.data["Oblast"]) | osm_df["state"].isnull()]
+        if self.data["Location"]["lat"] < CustomOptions.GEO_DATA["north"] and self.data["Location"]["lat"] > CustomOptions.GEO_DATA["south"] and self.data["Location"]["lon"] < CustomOptions.GEO_DATA["east"] and self.data["Location"]["lon"] > CustomOptions.GEO_DATA["west"]:      
+            try:
+                osm_df = osm_df.query(f'lat >= {self.data["Location"]["lat"]-CustomOptions.GEO_APPROX} and lat <= {self.data["Location"]["lat"]+CustomOptions.GEO_APPROX}')
+                osm_df = osm_df.query(f'lon >= {self.data["Location"]["lon"]-CustomOptions.GEO_APPROX} and lon <= {self.data["Location"]["lon"]+CustomOptions.GEO_APPROX}')
+                lat, lon = self.data["Location"]["lat"], self.data["Location"]["lon"]
+                locations = [(frame.lon-lon)**2+(frame.lat-lat)**2 for frame in osm_df.itertuples()]                
+                min_i = locations.index(min(locations))
+                city_list = re.findall(r"([А-ЩЮЯҐЄЇІ]*[а-щюяяґєїьі']+-)*([А-ЩЮЯҐЄЇІ][а-щюяяґєїьі'-]+,)", ",".join([str(v) for v in osm_df.values[min_i]]+[""]))
+                city_list = [("".join(city)).replace(',', '') for city in city_list]
+                print(city_list)
+                self.data["City"] = city_list
+            except:
+                Message(CustomOptions.MESSAGE_ERROR, "Вашу локацію не знайдено в межах України(поблизу вказаних кординат відсутні населені пунтки)", self.styleSheet())
+        else: 
+            Message(CustomOptions.MESSAGE_ERROR, "Ваша локація знаходиться за межами України)", self.styleSheet())
 
     @QC.Slot()
     def weather_menu(self):
         self.info_layout.setCurrentIndex(0)
-        self.weather.inputForm()        
+        self.weather.inputForm()
         self.weather.button_index.clicked.connect(self.check_index)
         self.setWindowTitle(CustomOptions.MAIN_NAME + ": " + CustomOptions.WEATHER_NAME)
 
@@ -132,13 +138,24 @@ class MainWindow(QW.QWidget):
     @QC.Slot()
     def history_load(self):
         if self.history.list.currentRow() == -1 and self.history.list.count() > 0:
-            Message(CustomOptions.MESSAGE_ERROR, "Ви нічого не вибрали(")
+            Message(CustomOptions.MESSAGE_ERROR, "Ви нічого не вибрали(", self.styleSheet())
         if self.history.list.currentRow() >= 0:
             self.info_layout.setCurrentIndex(0)
-            self.weather.generateLayout(self.history.load_data[self.history.list.currentRow()]["Weather"])
-            date = ".".join(self.history.load_data[self.history.list.currentRow()]["Weather"][0].split('-')[::-1])
-            self.setWindowTitle(f'{CustomOptions.MAIN_NAME} : {CustomOptions.WEATHER_NAME} - {self.history.load_data[self.history.list.currentRow()]["City"]} ({date})')
+            current_row = lambda: len(self.history.load_data) - self.history.list.currentRow() - 1
+            self.weather.generateLayout(self.history.load_data[current_row()]["Weather"])
+            date = ".".join(self.history.load_data[current_row()]["Weather"][0].split('-')[::-1])
+            self.setWindowTitle(f'{CustomOptions.MAIN_NAME} : {CustomOptions.WEATHER_NAME} - {self.history.load_data[current_row()]["City"]} ({date})')
         
+    @QC.Slot()
+    def history_delete(self):
+        if self.history.list.currentRow() == -1 and self.history.list.count() > 0:
+            Message(CustomOptions.MESSAGE_ERROR, "Ви нічого не вибрали(", self.styleSheet())
+        if self.history.list.currentRow() >= 0:
+            current_row = lambda: len(self.history.load_data) - self.history.list.currentRow() - 1
+            del self.history.load_data[current_row()]
+            self.history.saveHistory()
+            self.history.readHistory()
+            self.history.generaleList()
 
     @QC.Slot()
     def about_menu(self):
@@ -147,7 +164,7 @@ class MainWindow(QW.QWidget):
 
     @QC.Slot()
     def close_menu(self):
-        QW.QApplication.exit()  
+        QW.QApplication.exit()
 
     @QC.Slot()
     def check_index(self):
@@ -160,36 +177,45 @@ class MainWindow(QW.QWidget):
             except:
                 Message(CustomOptions.MESSAGE_ERROR, "Невірно уведені дані", self.styleSheet())
             if len(self.data["City"]) > 0:
-                del self.weather.form
-                self.data["Oblast_UA"] = self.data["City"]["Oblast"].values[0] + " Область"
-                self.data["Oblast"] = CustomOptions.DICTIONARY[self.data["City"]["Oblast"].values[0]] + " Oblast"
                 self.data["City"] = '-'.join(self.data["City"]["City"].values[0].split(' ')[1:])
-                print(f'{self.data["Oblast"]} {self.data["City"].lower()}')
-                self.getWeather(self.weather.radio_type)                
+                if self.getWeather(self.weather.radio_type):
+                    del self.weather.form
             else:
+                self.data["Index"] = ""
                 Message(CustomOptions.MESSAGE_ERROR, "Індекс не знайдено", self.styleSheet())
 
         #Seeking city
         if self.weather.radio_type == "city":
             self.data["City"] = '-'.join(self.weather.edit_index.text().split(' '))
-            del self.weather.form
-            print(f'{self.data["City"].lower()}')
-            if len(self.data["City"]) > 0:    
-                self.getWeather(self.weather.radio_type)            
+            if len(self.data["City"]) > 0:
+                if self.getWeather(self.weather.radio_type):
+                    del self.weather.form
+                else:
+                    self.data["City"] = ""
             
-        try:
-            self.data["Index"] = str(int(self.data["Index"]))
-        except:
-            self.data["Index"] = ""
+        #Seeking location
+        if self.weather.radio_type == "location":
+            self.data["Location"] = self.weather.edit_index.text().replace(" ", "").split(",")
+            try:
+                if len(self.data["Location"]) == 2:
+                    self.data["Location"] = {"lat":float(self.data["Location"][0]), "lon":float(self.data["Location"][1])}
+                    print(self.data["Location"])
+                    if self.getWeather(self.weather.radio_type):
+                        del self.weather.form
+                else: raise
+            except:
+                self.data["Location"] = ""
+                Message(CustomOptions.MESSAGE_ERROR, "Введіть 2 значення через кому в десятковому форматі, використовуючи крапку в якості роздільника", self.styleSheet())
 
         if len(self.data["Weather"]) > 0:
             self.history.saveHistory(self.data)
+            self.history.readHistory()
+            self.history.generaleList()
             self.weather.generateLayout(self.data["Weather"])
             self.setWindowTitle(CustomOptions.MAIN_NAME + ": " + CustomOptions.WEATHER_NAME + " - " + self.data["City"])
 
         print(self.data)
-            
-        
+
 
 
 if __name__ == "__main__":
